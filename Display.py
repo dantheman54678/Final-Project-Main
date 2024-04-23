9725841bd9cbe77c216c708c5bd5227f2d9f514f
ArronYoumans <166618174+ArronYoumans@users.noreply.github.com>
2024-04-23 19:19:59 -0400

    Update Display.py
    I added the start menu (minus background) plus finished the functionality of the Sudoku board.
    
    What remains are the buttons for resetting, restarting, and exiting on the sudoku board, the game over screen, the win screen, small details for the start screen, and the arrow key functions

--- Display.py
+++ Display.py
@@ -12,6 +12,8 @@ White = (255, 255, 255)
 Black = (0, 0, 0)
 Blue = (173, 216, 230)
 Red = (255, 0, 0)
+Orange = (255,165,0)
+Grey = (128,128,128)
 
 #initialize pygame
 p.font.init()
@@ -19,6 +21,69 @@ p.display.init()
 screen = p.display.set_mode((WIDTH,HEIGHT))
 p.display.set_caption("Sudoku")
 
+#Start screen
+def start_screen(win):
+    #Init screen
+    win.fill(White)
+
+    #Init fonts
+    title_font = p.font.Font(None, 100)
+    game_select_font = p.font.Font(None, 70)
+    button_font = p.font.Font(None, 50)
+
+    #Init title
+    title = title_font.render("Welcome to Sudoku",0,Black)
+    title_space = title.get_rect(center = (WIDTH//2, HEIGHT // 4))
+
+    #Init select text
+    game_select = game_select_font.render("Select Game Mode:",0,Black)
+    select_space = game_select.get_rect(center = (WIDTH // 2, (2*HEIGHT // 3)))
+
+    #Init easy button
+    easy_button = button_font.render("Easy", 0, White)
+    easy_surface = p.Surface((easy_button.get_size()[0] + 30, easy_button.get_size()[1] + 30))
+    easy_surface.fill(Orange)
+    easy_surface.blit(easy_button, (15,15))
+    easy_rect = easy_button.get_rect(center = (WIDTH//4, (HEIGHT // 4 + HEIGHT // 2)))
+
+    #Init medium button
+    medium_button = button_font.render("Medium", 0, White)
+    medium_surface = p.Surface((medium_button.get_size()[0] + 30, medium_button.get_size()[1] + 30))
+    medium_surface.fill(Orange)
+    medium_surface.blit(medium_button, (15,15))
+    medium_rect = medium_button.get_rect(center = (WIDTH//2, (HEIGHT // 4 + HEIGHT // 2)))
+
+    #Init hard button
+    hard_button = button_font.render("Hard", 0, White)
+    hard_surface = p.Surface((hard_button.get_size()[0] + 30, hard_button.get_size()[1] + 30))
+    hard_surface.fill(Orange)
+    hard_surface.blit(hard_button, (15,15))
+    hard_rect = hard_button.get_rect(center = ((WIDTH//2 + WIDTH // 4), (HEIGHT // 4 + HEIGHT // 2)))
+
+    #Draws all
+    screen.blit(title, title_space)
+    screen.blit(game_select, select_space)
+    screen.blit(easy_surface, easy_rect)
+    screen.blit(medium_surface, medium_rect)
+    screen.blit(hard_surface, hard_rect)
+
+    #Screen start up
+    while True:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                return "Quit"
+            if event.type == p.MOUSEBUTTONDOWN:
+                if easy_rect.collidepoint(event.pos):
+                    removed = 30
+                    return removed
+                elif medium_rect.collidepoint(event.pos):
+                    removed = 40
+                    return removed
+                elif hard_rect.collidepoint(event.pos):
+                    removed = 50
+                    return removed
+            p.display.update()
+            
 #Creates board
 class Board:
     def __init__(self):
@@ -68,11 +133,26 @@ class Board:
                 number.calc_position()
                 number.draw_number(win)
 
-    def draw_number(self, row, col, value, win):
-        font = p.font.SysFont('Times New Roman', 70)
-        number_text = font.render(str(value), True, Black)
-        win.blit(number_text, ((SQUARE_SIZE * col) + 38, (SQUARE_SIZE * row) + 12))
-        
+     def draw_sketch(self, blank_grid, win):
+        for row in range(ROWS):
+            for col in range(COLS):
+                value = blank_grid[row][col]
+                number = Cell(value,row,col)
+                number.draw_sketch(win)
+                
+    def draw_border(self, win, row=4, col=4):
+        if row == 3 or row == 6:
+            if col == 3 or col == 6:
+                p.draw.rect(win, Red, ((SQUARE_SIZE*col)+11,(SQUARE_SIZE*row)+11,SQUARE_SIZE-9,SQUARE_SIZE-9), 3)
+                        
+            else:
+                p.draw.rect(win, Red, ((SQUARE_SIZE*col)+6,(SQUARE_SIZE*row)+11,SQUARE_SIZE-6,SQUARE_SIZE-9), 3)
+                
+        elif col == 3 or col == 6:
+            p.draw.rect(win, Red, ((SQUARE_SIZE*col)+11,(SQUARE_SIZE*row)+6,SQUARE_SIZE-9,SQUARE_SIZE-6), 3)
+                
+        else:
+            p.draw.rect(win, Red, ((SQUARE_SIZE*col)+6,(SQUARE_SIZE*row)+6,SQUARE_SIZE-6,SQUARE_SIZE-6), 3)
 
 class Cell:
     def __init__(self,value,row,col):
@@ -102,22 +182,34 @@ def get_cordinates(pos):
     row = y // SQUARE_SIZE
     global col
     col = x // SQUARE_SIZE
-        
 
-    
+def check_completed(complete_board, grid):
+    for row in range(ROWS):
+        for col in range (COLS):
+            if grid[row][col] == 0:
+                return "pending"
+    if completed_board == grid:
+        return "winner"
+    if completed_board != grid:
+        return "game over" 
 
 def main():
-    run = True
+   run = True
+    blank_grid = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
     clock = p.time.Clock()
     board = Board()
-    grid = generate_sudoku(30,9)
+    clock.tick(FPS)
+    removed = start_screen(screen)
+    if removed == "Quit":
+        run = False
+    else:
+        complete_board, grid = generate_sudoku(removed,9)
+    do_it = 'no'
     condition = True
-    
-    
+    status = "pending"
+
     while run:
         value = 'None'
-        
-        clock.tick(FPS)
 
         for event in p.event.get():
             if event.type == p.QUIT:
@@ -126,33 +218,71 @@ def main():
             if event.type == p.MOUSEBUTTONDOWN:
                 pos = p.mouse.get_pos()
                 get_cordinates(pos)
-
+                do_it = "yes"
+                
             if event.type == p.KEYDOWN:
+                 
+                #Arrow key functions need to be added here!
+                
                 if event.key == p.K_1:
-                    value = 1
+                    val = 1
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_2:
-                    value = 2
+                    val = 2
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_3:
-                    value = 3
+                    val = 3
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_4:
-                    value = 4
+                    val = 4
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_5:
-                    value = 5
+                    val = 5
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_6:
-                    value = 6
+                    val = 6
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_7:
-                    value = 7
+                    val = 7
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_8:
-                    value = 8
+                    val = 8
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
                 if event.key == p.K_9:
-                    value = 9
-                if event.key == p.K_BACKSPACE:
-                    value = 0
+                    val = 9
+                    if grid[row][col] == 0:
+                        blank_grid[row][col] = val
+                if event.key == p.K_RETURN:
+                    value = blank_grid[row][col]
+                    
         if value != 'None':
             grid[row][col]=value
+            blank_grid[row][col] = 0
+
+        status = check_completed(complete_board, grid)
+        if status != "pending":
+            if status == "winner":
+                pass
+                #Draw win screen
+            else:
+                pass
+                #Draw game over screen
+        
         
         board.draw_squares(screen)
         board.draw_numbers(grid, screen)
+        board.draw_sketch(blank_grid, screen)
+        if do_it == "yes":
+            board.draw_border(screen, row, col)
+            
         p.display.update()
     p.quit()
 
